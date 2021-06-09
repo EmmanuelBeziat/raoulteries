aafeb10aa4d47649c3fad0845b0e762c65fde91e
Emmanuel Béziat <social@emmanuelbeziat.com>
2021-06-09 05:22:45 +0200

    V1

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const draw = () => {
+	const ctx = document.getElementById('canvas').getContext('2d')
+	const img = document.getElementById('model')
+	const text = document.getElementById('text')
+	const size = document.getElementById('size')
+	const save = document.getElementById('save')
+	const sizePreview = document.querySelector('.text-size-preview')
+	sizePreview.textContent = size.value
+
+	ctx.canvas.width = img.width
+	ctx.canvas.height = img.height
+	img.onload = () => {
+		ctx.drawImage(img, 0, 0, img.width, img.height)
+	}
+	img.src = './assets/img/raoult-kenobi.jpg'
+
+	const redraw = () => {
+		ctx.font = `${size.value}pt Arial`
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+		ctx.drawImage(img, 0, 0, img.width, img.height)
+		ctx.fillStyle = 'white'
+		ctx.textAlign = 'center'
+		ctx.fillText(text.value, (img.width / 2), (img.height - 40))
+	}
+
+	text.addEventListener('input', () => redraw())
+	size.addEventListener('input', () => {
+		redraw()
+		sizePreview.textContent = size.value
+	})
+
+	save.addEventListener('click', event => {
+		event.target.href = ctx.canvas.toDataURL('image/jpeg')
+	}, false)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	draw()
+})
